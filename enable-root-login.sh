255de27db0f7d6c7050d6e2f8aca42d3af6420bd
prashantsakharkar <38320006+prashantsakharkar@users.noreply.github.com>
2019-12-23 14:34:55 +0530

    update enable root password

--- enable-root-login.sh
+++ enable-root-login.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-
-sudo su
-echo "***************permit root login****************"
+#sudo su
+#echo "***************permit root login****************"
 sed -i '/#PermitRootLogin/c PermitRootLogin yes' /etc/ssh/sshd_config
 sed -i '/^PasswordAuthentication/c PasswordAuthentication yes' /etc/ssh/sshd_config
 sed -i '/^GSSAPIAuthentication/c GSSAPIAuthentication yes' /etc/ssh/sshd_config
-echo 'AllowUsers root' >> /etc/ssh/sshd_config
+#echo 'AllowUsers root' >> /etc/ssh/sshd_config
 echo "**************service sshd start****************"
+yes password | sudo passwd root
 service ssh restart
-echo -e "password\npassword" | passwd root
