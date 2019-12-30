7fc8d5b6bc740ec976fd5e571422b99d1183e0a5
prashantsakharkar <38320006+prashantsakharkar@users.noreply.github.com>
2019-12-30 16:15:19 +0530

    disabling iscsi socket

--- epel.sh
+++ epel.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo "10.10.10..213    controller" > /etc/hosts
 echo "192.168.14.213	controller" > /etc/hosts
+systemctl stop iscsid.socket
+systemctl disable iscsid.socket
 apt-get update
 apt-get install python-pip -y
 pip install -U pip
