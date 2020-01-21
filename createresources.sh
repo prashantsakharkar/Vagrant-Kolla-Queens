cbc131a1873ea32c6d569fdb4aff86ab81dc3c1e
Savitha Peri <savitha.peri@trilio.io>
2020-01-21 16:10:12 +0530

    Update createresources.sh

--- createresources.sh
+++ createresources.sh
@@ -22,7 +22,7 @@ openstack user create --domain trilio-test-domain --email trilio.build@trilio.io
 openstack project create --domain trilio-test-domain --description trilio-test-project-1 --enable trilio-test-project-1
 openstack project create --domain trilio-test-domain --description trilio-test-project-2 --enable trilio-test-project-2
 openstack quota set --backups 100 --cores 100 --instances 100 --snapshots 100 --volumes 100 --secgroups 100 --secgroup-rules 1000 trilio-test-project-1
- openstack quota set --backups 100 --cores 100 --instances 100 --snapshots 100 --volumes 100 --secgroups 100 --secgroup-rules 1000 trilio-test-project-2
+openstack quota set --backups 100 --cores 100 --instances 100 --snapshots 100 --volumes 100 --secgroups 100 --secgroup-rules 1000 trilio-test-project-2
 
 openstack role add --domain default --user admin admin
 openstack role add --user trilio-admin-user --project trilio-test-project-1 admin
@@ -36,13 +36,37 @@ openstack role add --user trilio-test-user --domain trilio-test-domain admin
 openstack network create --enable --project trilio-test-project-1 --internal trilio-internal-network
 openstack subnet create --project trilio-test-project-1 --subnet-range 25.25.1.0/24 --ip-version 4 --network trilio-internal-network trilio-internal-subnet
 
-openstack network create --enable --share --external public_network
-openstack subnet create --gateway 192.168.1.1 --ip-version 4 --network public_network --allocation-pool start=192.168.40.5,end=192.168.40.10 --dhcp --subnet-range 192.168.0.0/16 public_subnet
+openstack network create --share --external --enable --provider-network-type flat --provider-physical-network physnet1 public_network
+openstack subnet create --gateway 192.168.1.1 --ip-version 4 --network public_network --allocation-pool start=192.168.40.5,end=192.168.40.10 --no-dhcp --subnet-range 192.168.0.0/16 public_subnet
+openstack router create --enable --project trilio-test-project-1 test_router
 
-wget http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img
-openstack image create cirros --file cirros-0.4.0-x86_64-disk.img --disk-format qcow2 --container-format bare --public
+int_network_id=`(openstack network list --internal | awk -F'|' '!/^(+--)|ID|aki|ari/ { print $2 }'| awk '{$1=$1;print}')`
+ext_network_id=`(openstack network list --external | awk -F'|' '!/^(+--)|ID|aki|ari/ { print $2 }'| awk '{$1=$1;print}')`
+router_id=`(openstack router list | grep test_router | awk '$2 && $2 != "ID" {print $2}')`
+subnet_id=`(openstack subnet list | grep $int_network_id | awk '$2 && $2 != "ID" {print $2}')`
+
+openstack router set --external-gateway $ext_network_id test_router
+openstack router add subnet test_router $subnet_id
+
+#Allocate floating ips to test project
+floating_ip_count=12
+i=0
+while [ $i -lt $floating_ip_count ]
+do
+   i=`expr $i + 1`
+   openstack floating ip create --project trilio-test-project-1 $ext_network_id
+done
+
+#wget http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img
+#openstack image create cirros --file cirros-0.4.0-x86_64-disk.img --disk-format qcow2 --container-format bare --public
+#rm -f cirros-0.4.0-x86_64-disk.img
+
+wget https://osm-download.etsi.org/ftp/osm-3.0-three/1st-hackfest/images/cirros-0.3.4-x86_64-disk.img
+openstack image create cirros --file cirros-0.3.4-x86_64-disk.img --disk-format qcow2 --container-format bare --public
+rm -f cirros-0.3.4-x86_64-disk.img
 
 openstack volume type create --public --property volume_backend_name=lvm-1 lvm
+openstack volume type create --public --property volume_backend_name=ceph ceph
 
 openstack flavor create --ram 64 --disk 1 --vcpus 1 tiny
 
@@ -50,6 +74,7 @@ chown root:root /opt/file-manager/tvault-recoverymanager-2.3.2.qcow2.gz
 gunzip /opt/file-manager/tvault-recoverymanager-2.3.2.qcow2.gz
 openstack image create fvm --file /opt/file-manager/tvault-recoverymanager-2.3.2.qcow2 --disk-format qcow2 --container-format bare --public
 openstack image set --property hw_qemu_guest_agent=yes fvm
+rm -rf /opt/file-manager
 
 def_secgrp_id=`(openstack security group list --project trilio-test-project-1 | awk -F'|' '!/^(+--)|ID|aki|ari/ { print $2 }')`
 echo $def_secgrp_id
@@ -59,18 +84,11 @@ openstack security group rule create --ethertype IPv4 --egress --protocol tcp --
 openstack security group rule create --ethertype IPv4 --ingress --protocol icmp $def_secgrp_id
 openstack security group rule create --ethertype IPv4 --egress --protocol icmp $def_secgrp_id
 
-#Enable multidomain for Horizon
-sed -i '/OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = /c OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = True' /etc/kolla/horizon/local_settings
-docker stop horizon
-docker start horizon
-sleep 10s
-
 #Enable cloud admin
-service_tenant_id=`(openstack project list | grep service | awk -F'|' '!/^(+--)|ID|aki|ari/ { print $2 }'| awk '{$1=$1;print}')`
 cloudadmin_domain_id=`(openstack domain list | grep clouddomain | awk -F'|' '!/^(+--)|ID|aki|ari/ {print $2}' | awk '{$1=$1;print}')`
 cloud_project_id=`(openstack project list | grep cloudproject | awk -F'|' '!/^(+--)|ID|aki|ari/ {print $2}' | awk '{$1=$1;print}')`
 
-#Create cloudrc file on openstack at /etc/kolla/ directory
+#Create cloudrc file on openstack at /etc/kolla directory
 echo "export OS_AUTH_URL=$OS_AUTH_URL
 export OS_PROJECT_ID=$cloud_project_id
 export OS_PROJECT_NAME=cloudproject
