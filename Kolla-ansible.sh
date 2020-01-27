0d1e6c5eaf92361ca91ad3e255eb98f33a1514e4
Prashant Sakharkar <prashantsakharkar.2010@gmail.com>
2020-01-27 03:06:48 -0500

    Train Changes

--- Kolla-ansible.sh
+++ Kolla-ansible.sh
@@ -4,7 +4,7 @@ vgcreate cinder-volumes /dev/vda
 echo "nameserver 172.172.3.1" >> /etc/resolve.conf
 echo "nameserver 8.8.8.8" >> /etc/resolve.conf
 mkdir /etc/kolla/config
-pip install "kolla-ansible==6.2.1"
+pip install "kolla-ansible==9.0.0"
 cp -r /usr/local/share/kolla-ansible/etc_examples/kolla /etc/kolla/
 cp /usr/local/share/kolla-ansible/ansible/inventory/* .
 mkdir /etc/kolla/certificates/
@@ -15,14 +15,14 @@ sed -i 's/1048576//g' /usr/local/share/kolla-ansible/ansible/roles/neutron/tasks
 
 echo "kolla_base_distro: "ubuntu"
 kolla_install_type: "binary"
-openstack_release: "queens"
+openstack_release: "train"
 kolla_internal_vip_address: "10.10.10.214"
 kolla_external_vip_address: "172.172.3.203"
 kolla_external_fqdn: "kollaautoqueen.triliodata.demo"
 kolla_enable_tls_external: "yes"
 kolla_external_fqdn_cert: "/etc/kolla/certificates/tvm.cert.pem"
 kolla_external_fqdn_cacert: "/etc/kolla/certificates/tvm.cert.pem"
-network_interface: "eth1"
+network_interface: "eth2"
 neutron_external_interface: "eth3"
 keepalived_virtual_router_id: "95"
 openstack_region_name: "USWEST"
@@ -38,6 +38,8 @@ kolla-genpwd
 
 secret=`cat /etc/kolla/passwords.yml | grep cinder_rbd | awk -F ' ' '{print $2}'`
 sed  -i '/rbd_secret_uuid/c rbd_secret_uuid = '$secret /etc/kolla/config/cinder/cinder-volume.conf
+
+kolla-ansible -i /home/vagrant/all-in-one bootstrap-servers -vvv
 
 kolla-ansible -i /home/vagrant/all-in-one prechecks -vvv
 
--- epel.sh
+++ epel.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-echo "10.10.10..213    controller" >> /etc/hosts
-echo "172.172.3.202	controller" >> /etc/hosts
+echo "10.10.10.213    controller" >> /etc/hosts
 systemctl stop iscsid.socket
 systemctl disable iscsid.socket
 apt-get update
